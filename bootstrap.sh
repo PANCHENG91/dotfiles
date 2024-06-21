0ea9707cf3bf4d967b8d6aa61dc6abc103c682c5
pancheng <pancheng@hisense.com>
2024-06-21 22:08:12 +0800

    feat(util-macos-adapt)

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,14 +13,21 @@ function doIt() {
 		-avh --no-perms . ~
 
 	if [[ $(uname -s) = Darwin ]]; then
-		rm -rf ~/soft_pc/macos/nvim-macos-arm64
-		xattr -c ~/soft_pc/macos/nvim-macos-arm64.tar.gz
-		tar zxf ~/soft_pc/macos/nvim-macos-arm64.tar.gz -C ~/soft_pc/macos/
+		rm -rf ~/soft_pc/macos/nvim-macos-arm64 && xattr -c ~/soft_pc/macos/nvim-macos-arm64.tar.gz && tar zxf ~/soft_pc/macos/nvim-macos-arm64.tar.gz -C ~/soft_pc/macos/ && rm ~/soft_pc/macos/nvim-macos-arm64.tar.gz
+		rm -rf ~/soft_pc/macos/lazygit/ && mkdir -p ~/soft_pc/macos/lazygit && xattr -c ~/soft_pc/macos/lazygit_0.42.0_Darwin_arm64.tar.gz &&
+			tar zxf ~/soft_pc/macos/lazygit_0.42.0_Darwin_arm64.tar.gz -C ~/soft_pc/macos/lazygit && rm ~/soft_pc/macos/lazygit_0.42.0_Darwin_arm64.tar.gz
+		rm -rf ~/soft_pc/macos/fd-v10.1.0-aarch64-apple-darwin && xattr ~/soft_pc/macos/fd-v10.1.0-aarch64-apple-darwin.tar.gz &&
+			tar zxf ~/soft_pc/macos/fd-v10.1.0-aarch64-apple-darwin.tar.gz -C ~/soft_pc/macos && rm ~/soft_pc/macos/fd-v10.1.0-aarch64-apple-darwin.tar.gz
+		rm -rf ~/soft_pc/macos/ripgrep-14.1.0-aarch64-apple-darwin && xattr ~/soft_pc/macos/ripgrep-14.1.0-aarch64-apple-darwin.tar.gz &&
+			tar zxf ~/soft_pc/macos/ripgrep-14.1.0-aarch64-apple-darwin.tar.gz -C ~/soft_pc/macos/ && rm ~/soft_pc/macos/ripgrep-14.1.0-aarch64-apple-darwin.tar.gz
 	elif [[ $(uname -s) = Linux ]]; then
 		rm -rf ~/soft_pc/linux/nvim-linux64 && tar xzf ~/soft_pc/linux/nvim-linux64.tar.gz -C ~/soft_pc/linux/ && rm ~/soft_pc/linux/nvim-linux64.tar.gz
-		rm -rf ~/soft_pc/linux/lazygit/ && mkdir -p ~/soft_pc/linux/lazygit && tar zxf ~/soft_pc/linux/lazygit_0.42.0_Linux_x86_64.tar.gz -C ~/soft_pc/linux/lazygit/ && rm ~/soft_pc/linux/lazygit_0.42.0_Linux_x86_64.tar.gz
-		rm -rf ~/soft_pc/linux/fd-v10.1.0-x86_64-unknown-linux-gnu && tar zxf ~/soft_pc/linux/fd-v10.1.0-x86_64-unknown-linux-gnu.tar.gz -C ~/soft_pc/linux/ && rm ~/soft_pc/linux/fd-v10.1.0-x86_64-unknown-linux-gnu.tar.gz
-		rm -rf ~/soft_pc/linux/ripgrep-14.1.0-x86_64-unknown-linux-musl && tar zxf ~/soft_pc/linux/ripgrep-14.1.0-x86_64-unknown-linux-musl.tar.gz -C ~/soft_pc/linux/ && rm ~/soft_pc/linux/ripgrep-14.1.0-x86_64-unknown-linux-musl.tar.gz
+		rm -rf ~/soft_pc/linux/lazygit/ && mkdir -p ~/soft_pc/linux/lazygit && tar zxf ~/soft_pc/linux/lazygit_0.42.0_Linux_x86_64.tar.gz -C ~/soft_pc/linux/lazygit/ &&
+			rm ~/soft_pc/linux/lazygit_0.42.0_Linux_x86_64.tar.gz
+		rm -rf ~/soft_pc/linux/fd-v10.1.0-x86_64-unknown-linux-gnu && tar zxf ~/soft_pc/linux/fd-v10.1.0-x86_64-unknown-linux-gnu.tar.gz -C ~/soft_pc/linux/ &&
+			rm ~/soft_pc/linux/fd-v10.1.0-x86_64-unknown-linux-gnu.tar.gz
+		rm -rf ~/soft_pc/linux/ripgrep-14.1.0-x86_64-unknown-linux-musl && tar zxf ~/soft_pc/linux/ripgrep-14.1.0-x86_64-unknown-linux-musl.tar.gz -C ~/soft_pc/linux/ &&
+			rm ~/soft_pc/linux/ripgrep-14.1.0-x86_64-unknown-linux-musl.tar.gz
 	fi
 
 	if [[ $SHELL = *"bash" ]]; then
