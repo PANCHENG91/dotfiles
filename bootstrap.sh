5131e0c760b7efd209848b023f9ce09188390bb1
pancheng <pancheng@hisense.com>
2025-06-12 15:09:26 +0800

    feat(update nvim version)

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,11 @@ cd "$(dirname "${BASH_SOURCE}")"
 #git pull origin main;
 
 function doIt() {
+  if [[ $(uname -s) = Darwin ]]; then
+    rm -rf ~/soft_pc/macos/*
+  elif [[ $(uname -s) = Linux ]]; then
+    rm -rf ~/soft_pc/linux/*
+  fi
   rsync --exclude ".git" \
     --exclude "bootstrap.sh" \
     --exclude "README.md" \
@@ -13,25 +18,25 @@ function doIt() {
     -avh --no-perms . ~
 
   if [[ $(uname -s) = Darwin ]]; then
-    rm -rf ~/soft_pc/macos/nvim-macos-arm64 && xattr -c ~/soft_pc/macos/nvim-macos-arm64.tar.gz && tar zxf ~/soft_pc/macos/nvim-macos-arm64.tar.gz -C ~/soft_pc/macos/ && rm ~/soft_pc/macos/nvim-macos-arm64.tar.gz
-    rm -rf ~/soft_pc/macos/lazygit/ && mkdir -p ~/soft_pc/macos/lazygit && xattr -c ~/soft_pc/macos/lazygit_0.42.0_Darwin_arm64.tar.gz &&
+    xattr -c ~/soft_pc/macos/nvim-macos-arm64.tar.gz && tar zxf ~/soft_pc/macos/nvim-macos-arm64.tar.gz -C ~/soft_pc/macos/ && rm ~/soft_pc/macos/nvim-macos-arm64.tar.gz
+    mkdir -p ~/soft_pc/macos/lazygit && xattr -c ~/soft_pc/macos/lazygit_0.42.0_Darwin_arm64.tar.gz &&
       tar zxf ~/soft_pc/macos/lazygit_0.42.0_Darwin_arm64.tar.gz -C ~/soft_pc/macos/lazygit && rm ~/soft_pc/macos/lazygit_0.42.0_Darwin_arm64.tar.gz
-    rm -rf ~/soft_pc/macos/fd-v10.1.0-aarch64-apple-darwin && xattr ~/soft_pc/macos/fd-v10.1.0-aarch64-apple-darwin.tar.gz &&
+    xattr ~/soft_pc/macos/fd-v10.1.0-aarch64-apple-darwin.tar.gz &&
       tar zxf ~/soft_pc/macos/fd-v10.1.0-aarch64-apple-darwin.tar.gz -C ~/soft_pc/macos && rm ~/soft_pc/macos/fd-v10.1.0-aarch64-apple-darwin.tar.gz
-    rm -rf ~/soft_pc/macos/ripgrep-14.1.0-aarch64-apple-darwin && xattr ~/soft_pc/macos/ripgrep-14.1.0-aarch64-apple-darwin.tar.gz &&
+    xattr ~/soft_pc/macos/ripgrep-14.1.0-aarch64-apple-darwin.tar.gz &&
       tar zxf ~/soft_pc/macos/ripgrep-14.1.0-aarch64-apple-darwin.tar.gz -C ~/soft_pc/macos/ && rm ~/soft_pc/macos/ripgrep-14.1.0-aarch64-apple-darwin.tar.gz
-    rm -rf ~/soft_pc/macos/fzf && xattr ~/soft_pc/macos/fzf-0.57.0-darwin_arm64.tar.gz &&
+    xattr ~/soft_pc/macos/fzf-0.57.0-darwin_arm64.tar.gz &&
       tar zxf ~/soft_pc/macos/fzf-0.57.0-darwin_arm64.tar.gz -C ~/soft_pc/macos/ && rm ~/soft_pc/macos/fzf-0.57.0-darwin_arm64.tar.gz
   elif [[ $(uname -s) = Linux ]]; then
-    rm -rf ~/soft_pc/linux/nvim-linux64 && tar xzf ~/soft_pc/linux/nvim-linux64.tar.gz -C ~/soft_pc/linux/ && rm ~/soft_pc/linux/nvim-linux64.tar.gz
-    rm -rf ~/soft_pc/linux/lazygit/ && mkdir -p ~/soft_pc/linux/lazygit && tar zxf ~/soft_pc/linux/lazygit_0.42.0_Linux_x86_64.tar.gz -C ~/soft_pc/linux/lazygit/ &&
+    tar xzf ~/soft_pc/linux/nvim-linux-x86_64.tar.gz -C ~/soft_pc/linux/ && rm ~/soft_pc/linux/nvim-linux-x86_64.tar.gz
+    mkdir -p ~/soft_pc/linux/lazygit && tar zxf ~/soft_pc/linux/lazygit_0.42.0_Linux_x86_64.tar.gz -C ~/soft_pc/linux/lazygit/ &&
       rm ~/soft_pc/linux/lazygit_0.42.0_Linux_x86_64.tar.gz
-    rm -rf ~/soft_pc/linux/fd-v10.1.0-x86_64-unknown-linux-gnu && tar zxf ~/soft_pc/linux/fd-v10.1.0-x86_64-unknown-linux-gnu.tar.gz -C ~/soft_pc/linux/ &&
+    tar zxf ~/soft_pc/linux/fd-v10.1.0-x86_64-unknown-linux-gnu.tar.gz -C ~/soft_pc/linux/ &&
       rm ~/soft_pc/linux/fd-v10.1.0-x86_64-unknown-linux-gnu.tar.gz
-    rm -rf ~/soft_pc/linux/ripgrep-14.1.0-x86_64-unknown-linux-musl && tar zxf ~/soft_pc/linux/ripgrep-14.1.0-x86_64-unknown-linux-musl.tar.gz -C ~/soft_pc/linux/ &&
+    tar zxf ~/soft_pc/linux/ripgrep-14.1.0-x86_64-unknown-linux-musl.tar.gz -C ~/soft_pc/linux/ &&
       rm ~/soft_pc/linux/ripgrep-14.1.0-x86_64-unknown-linux-musl.tar.gz
-    rm -rf ~/soft_pc/linux/fzf && tar zxf ~/soft_pc/linux/fzf-0.57.0-linux_amd64.tar.gz -C ~/soft_pc/linux/ && rm -rf ~/soft_pc/linux/fzf-0.57.0-linux_amd64.tar.gz
-    rm -rf ~/soft_pc/linux/yazi-x86_64-unknown-linux-gnu/ && unzip ~/soft_pc/linux/yazi-x86_64-unknown-linux-gnu.zip -d ~/soft_pc/linux/ && rm -rf ~/soft_pc/linux/yazi-x86_64-unknown-linux-gnu.zip
+    tar zxf ~/soft_pc/linux/fzf-0.57.0-linux_amd64.tar.gz -C ~/soft_pc/linux/ && rm -rf ~/soft_pc/linux/fzf-0.57.0-linux_amd64.tar.gz
+    unzip ~/soft_pc/linux/yazi-x86_64-unknown-linux-gnu.zip -d ~/soft_pc/linux/ && rm -rf ~/soft_pc/linux/yazi-x86_64-unknown-linux-gnu.zip
   fi
 
   if [[ $SHELL = *"bash" ]]; then
